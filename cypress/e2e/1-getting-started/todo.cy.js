4ed90a8265d977a3c68c0750c89669a523ff491e
mayar <mayarsamir82@gmail.com>
2025-05-02 01:02:34 +0300

    add more tests

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -14,4 +14,21 @@ describe('example to-do app', () => {
     cy.get('[data-test="product-name"]').contains('Claw Hammer').click()
     cy.get('[data-test="add-to-cart"]').should('be.visible')
   })
+
+  it('clicking a product navigates to product page', () => {
+    cy.get('[data-test="product-name"]').contains('Pliers').click()
+    cy.get('[data-test="add-to-cart"]').should('be.visible')
+  })
+  it('clicking a product navigates to product page', () => {
+    cy.get('[data-test="product-name"]').contains('Bolt Cutters').click()
+    cy.get('[data-test="add-to-cart"]').should('be.visible')
+  })
+
+  it('clicking a home button navigates to home page', () => {
+    cy.get('[data-test="nav-home"]').click()
+    cy.get('body').should('be.visible')
+    
+  })
+
+
 })
